c381a950d22061603763b27264dd0306c29289d7
ym99 <yuriy.myshkevych@gmail.com>
2017-01-20 09:49:13 -0500

    v1.1

---
+++ words.js
@@ -0,0 +1,38 @@
+﻿const newWords = [
+    { spanish: "echar", english: "to throw" },
+    { spanish: "salir", english: "to leave" },
+    { spanish: "secar", english: "to take out" },
+    { spanish: "venir", english: "to come" },
+    { spanish: "saber", english: "to know" },
+];
+
+const words = [
+    // -- AR ----------------------------
+    { spanish: "estar", english: "to be", englishComment: " (place, feeling)" },
+    { spanish: "pensar", english: "to think" },
+    { spanish: "llevar", english: "to wear" },
+    { spanish: "amar", english: "to love" },
+
+    { spanish: "gustar", english: "to please" },
+    { spanish: "dar", english: "to give" },
+    { spanish: "empezar", english: "to begin" },
+    { spanish: "andar", english: "to walk" },
+
+    { spanish: "trabajar", english: "to work" },
+    { spanish: "escuechar", english: "to listen" },
+
+    // -- ER ----------------------------
+    { spanish: "ser", english: "to be", englishComment: " (somebody)" },
+    { spanish: "hacer", english: "to do" },
+    { spanish: "haber", english: "to have" },
+//          { spanish: "tener", english: "to have" },
+    { spanish: "querer", english: "to want" },
+
+    { spanish: "poder", english: "to be able" },
+    { spanish: "poner", english: "to put" },
+    { spanish: "deber", english: "to owe" },
+    { spanish: "leer", english: "to read" },
+    { spanish: "comer", english: "to eat" },
+
+    { spanish: "beber", english: "to drink" },
+];
