76d10a0823fedd4f4677fb7086084808bd059666
ym99 <yuriy.myshkevych@gmail.com>
2017-01-23 20:17:54 -0500

    merge

--- app/words.jsx
+++ app/words.jsx
@@ -1,28 +1,36 @@
 export const words = {
     new: [
-        { spanish: "decir", english: "to say" },
-        { spanish: "creer", english: "to believe" },
-        { spanish: "encontrar", english: "to find" },
+        { spanish: "gracias", english: "thanks" },
+        { spanish: "bueno", english: "good" },
+        { spanish: "amigo", english: "friend" },
+        { spanish: "por favor", english: "please" },
+        { spanish: "ahora", english: "now" },
+        { spanish: "feliz", english: "happy" },
+        { spanish: "todo", english: "all" },
+        { spanish: "mismo", english: "same" },
+        { spanish: "muy", english: "very" },
+        { spanish: "nunca", english: "never" }
     ],
 
     old: [
-        // -- AR ----------------------------
+        // -- AR verb ----------------------------
         { spanish: "estar", english: "to be", englishComment: " (location, feeling)" },
         { spanish: "pensar", english: "to think" },
         { spanish: "llevar", english: "to wear" },
         { spanish: "amar", english: "to love" },
-
         { spanish: "gustar", english: "to like", englishComment: " (to be pleasing)" },
+
         { spanish: "dar", english: "to give" },
         { spanish: "empezar", english: "to begin" },
         { spanish: "andar", english: "to walk" },
-
         { spanish: "trabajar", english: "to work" },
         { spanish: "escuchar", english: "to listen" },
+
         { spanish: "echar", english: "to throw" },
         { spanish: "sacar", english: "to take out" },
+        { spanish: "encontrar", english: "to find" },
 
-        // -- ER ----------------------------
+        // -- ER verb ----------------------------
         { spanish: "ser", english: "to be", englishComment: " (somebody)" },
         { spanish: "hacer", english: "to do" },
         { spanish: "haber", english: "to have", englishComment: " (auxiliary, supportive verb)" },
@@ -42,10 +50,12 @@ export const words = {
         { spanish: "entender", english:"to understand" },
 
         { spanish: "ver", english: "to see" },
+        { spanish: "creer", english: "to believe" },
 
-        // -- IR ----------------------------
+        // -- IR vreb ----------------------------
         { spanish: "ir", english: "to go" },
         { spanish: "salir", english: "to leave" },
         { spanish: "venir", english: "to come" },
+        { spanish: "decir", english: "to say" },
     ]
 };
