36653d3dab73fa38a5972b7603f2f28817914166
ym99 <yuriy.myshkevych@gmail.com>
2017-01-23 20:05:12 -0500

    10 more words

--- docs/words.js
+++ docs/words.js
@@ -1,27 +1,35 @@
 ﻿const newWords = [
-    { spanish: "decir", english: "to say" },
-    { spanish: "creer", english: "to believe" },
-    { spanish: "encontrar", english: "to find" },
+    { spanish: "gracias", english: "thanks" },
+    { spanish: "bueno", english: "good" },
+    { spanish: "amigo", english: "friend" },
+    { spanish: "por favor", english: "please" },
+    { spanish: "ahora", english: "now" },
+    { spanish: "feliz", english: "happy" },
+    { spanish: "todo", english: "all" },
+    { spanish: "mismo", english: "same" },
+    { spanish: "muy", english: "very" },
+    { spanish: "nunca", english: "never" }
 ];
 
 const words = [
-    // -- AR ----------------------------
+    // -- AR verb ----------------------------
     { spanish: "estar", english: "to be", englishComment: " (location, feeling)" },
     { spanish: "pensar", english: "to think" },
     { spanish: "llevar", english: "to wear" },
     { spanish: "amar", english: "to love" },
-
     { spanish: "gustar", english: "to like", englishComment: " (to be pleasing)" },
+
     { spanish: "dar", english: "to give" },
     { spanish: "empezar", english: "to begin" },
     { spanish: "andar", english: "to walk" },
-
     { spanish: "trabajar", english: "to work" },
     { spanish: "escuchar", english: "to listen" },
+
     { spanish: "echar", english: "to throw" },
     { spanish: "sacar", english: "to take out" },
+    { spanish: "encontrar", english: "to find" },
 
-    // -- ER ----------------------------
+    // -- ER verb ----------------------------
     { spanish: "ser", english: "to be", englishComment: " (somebody)" },
     { spanish: "hacer", english: "to do" },
     { spanish: "haber", english: "to have", englishComment: " (auxiliary, supportive verb)" },
@@ -41,9 +49,11 @@ const words = [
     { spanish: "entender", english:"to understand" },
 
     { spanish: "ver", english: "to see" },
+    { spanish: "creer", english: "to believe" },
 
-    // -- IR ----------------------------
+    // -- IR vreb ----------------------------
     { spanish: "ir", english: "to go" },
     { spanish: "salir", english: "to leave" },
     { spanish: "venir", english: "to come" },
+    { spanish: "decir", english: "to say" },
 ];
