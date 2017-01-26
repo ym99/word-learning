96cff53e3ab1a4ec81aced63cebcde63b3152394
ym99 <yuriy.myshkevych@gmail.com>
2017-01-26 16:21:40 -0500

    fixed Info

--- app/Info.jsx
+++ app/Info.jsx
@@ -1,20 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export class Info extends Component  {
-    render(){
-        return this.props.stats.grade ? (
-                <div
-                    className="well"
-                    style={({
-                        position: 'fixed',
-                        right: '0px',
-                        top: '0px'
-                    })}
-                >
-                    <span className={this.props.stats.gradeClass} style={({fontSize: "large"})}>{this.props.stats.grade}</span>
-                    &nbsp;
-                    <span className="badge" style={({fontSize: "larger"})}>{this.props.stats.percentInfo}</span>
-                </div>
-        ) : null;
-    }
+export default class Info extends React.Component {
+  static propTypes = {
+    stats: React.PropTypes.shape({
+      grade: React.PropTypes.string.isRequired,
+      gradeClass: React.PropTypes.string.isRequired,
+      percentInfo: React.PropTypes.string.isRequired,
+    }).isRequired,
+  }
+
+  render() {
+    return this.props.stats.grade ? (
+      <div
+        className="well"
+        style={({
+          position: 'fixed',
+          right: '0px',
+          top: '0px',
+        })}
+      >
+        <span
+          className={this.props.stats.gradeClass}
+          style={({
+            fontSize: 'large',
+          })}
+        >{this.props.stats.grade}</span>
+        &nbsp;
+        <span
+          className="badge"
+          style={({
+            fontSize: 'larger',
+          })}
+        >{this.props.stats.percentInfo}</span>
+      </div>
+    ) : null;
+  }
 }
