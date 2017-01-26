fb089a6c9d6e895b15b27bbebb3f67b24720f3df
ym99 <yuriy.myshkevych@gmail.com>
2017-01-26 12:05:25 -0500

    moved bundle.js to docs/

--- dev-tools/webpack.config.js
+++ dev-tools/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "../temp"),
         filename: "bundle.js",
-        publicPath: '/static/'
+        publicPath: '/'
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
