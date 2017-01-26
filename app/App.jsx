bb2e52fa1978adb8fea7696c9e23a74f33225d3f
ym99 <yuriy.myshkevych@gmail.com>
2017-01-26 16:07:22 -0500

    jslint-ed App and words

--- app/App.jsx
+++ app/App.jsx
@@ -1,171 +1,184 @@
-import React, { Component } from 'react';
-import { Progress } from 'Progress';
-import { Info } from 'Info';
-import { History } from 'History';
-import { FinalInfo } from 'FinalInfo';
+/* eslint-disable no-nested-ternary */
+import React from 'react';
+import { Progress } from './Progress';
+import { Info } from './Info';
+import { History } from './History';
+import { FinalInfo } from './FinalInfo';
 
-export class App extends Component  {
-    constructor(props){
-        super(props);
+export default class App extends React.Component {
+  static propTypes = {
+    words: React.PropTypes.shape({
+      spanish: React.PropTypes.string.isRequired,
+      spanishComment: React.PropTypes.string,
+      english: React.PropTypes.string.isRequired,
+      englishComment: React.PropTypes.string,
+    }).isRequired,
+  }
 
-        this.switchTo  = this.switchTo.bind(this);
-        this.processAnswer = this.processAnswer.bind(this);
+  static generateQuestionIndex(questions) {
+    return questions.length === 0 ? null : Math.floor(Math.random() * questions.length);
+  }
 
-        const mode = "test";
-        const questions = App.generateQuestions(this.props, mode);
-        const questionIndex = App.generateQuestionIndex(questions);
-
-        this.state = {
-            mode,
-            questions,
-            questionIndex,
-            history: []
-        };
+  static generateQuestions({ words }, mode) {
+    function createSpanishQuestion(word) {
+      return {
+        text: word.spanish + (word.spanishComment || ''),
+        answer: word.english,
+      };
     }
 
-    static generateQuestionIndex(questions){
-        return questions.length === 0 ? null : Math.floor(Math.random() * questions.length);
+    function createEnglishQuestion(word) {
+      return {
+        text: word.english + (word.englishComment || ''),
+        answer: word.spanish,
+      };
     }
 
-    static generateQuestions({words}, mode){
-        function createSpanishQuestion(word) {
-            return {
-                text: word.spanish + (word.spanishComment || ""),
-                answer: word.english,
-            };
+    let newRatio;
+    let oldRatio;
+    switch (mode) {
+      case 'new':
+        newRatio = 10;
+        oldRatio = 0;
+        break;
+
+      case 'mix':
+        newRatio = 5;
+        oldRatio = 1;
+        break;
+
+      case 'test':
+        newRatio = 1;
+        oldRatio = 1;
+        break;
+
+      default:
+        newRatio = 1;
+        oldRatio = 1;
+        break;
+    }
+
+    const questions = [];
+
+    for (let i = 0; i < (words.new || []).length; i += 1) {
+      for (let c = 0; c < newRatio; c += 1) {
+        questions.push(createSpanishQuestion(words.new[i]));
+        questions.push(createEnglishQuestion(words.new[i]));
+      }
+    }
+
+    for (let i = 0; i < (words.old || []).length; i += 1) {
+      for (let c = 0; c < oldRatio; c += 1) {
+        questions.push(createSpanishQuestion(words.old[i]));
+        questions.push(createEnglishQuestion(words.old[i]));
+      }
+    }
+
+    return questions;
+  }
+
+  static switchTo(mode) {
+    return (prevState, props) => {
+      const questions = App.generateQuestions(props, mode);
+      const questionIndex = App.generateQuestionIndex(questions);
+
+      return {
+        mode,
+        questions,
+        questionIndex,
+        history: [],
+      };
+    };
+  }
+
+  static getStats({ history }) {
+    const total = history.length;
+    const correct = history.reduce((accum, record) => (accum + record.isCorrectAnswer ? 1 : 0), 0);
+    const percent = total === 0 ? null : correct / total;
+    const grade =
+      percent === null ? null :
+      percent >= 0.94 ? 'A' :
+      percent >= 0.90 ? 'A-' :
+      percent >= 0.87 ? 'B+' :
+      percent >= 0.83 ? 'B' :
+      percent >= 0.80 ? 'B-' :
+      percent >= 0.77 ? 'C+' :
+      percent >= 0.73 ? 'C' :
+      percent >= 0.70 ? 'C-' :
+      percent >= 0.67 ? 'D+' :
+      percent >= 0.60 ? 'D' :
+                        'F'
+    ;
+
+    return {
+      total,
+      correct,
+      percentInfo: percent === null ? '' : `${(percent * 100).toFixed(0)}% correct`,
+      grade,
+      gradeClass:
+        percent === null ? '' :
+        grade.startsWith('A') ? 'label label-success' :
+        grade.startsWith('B') ? 'label label-info' :
+        grade.startsWith('C') ? 'label label-warning' :
+                                'label label-danger',
+    };
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.processAnswer = this.processAnswer.bind(this);
+
+    const mode = 'test';
+    const questions = App.generateQuestions(this.props, mode);
+    const questionIndex = App.generateQuestionIndex(questions);
+
+    this.state = {
+      mode,
+      questions,
+      questionIndex,
+      history: [],
+    };
+  }
+
+  processAnswer(answer) {
+    this.setState((prevState) => {
+      const question = prevState.questions[prevState.questionIndex];
+
+      const newQuestions = [...prevState.questions];
+      newQuestions.splice(prevState.questionIndex, 1);
+
+      return {
+        questions: newQuestions,
+        questionIndex: App.generateQuestionIndex(newQuestions),
+        history: [...prevState.history, {
+          isCorrectAnswer: answer.trim().toUpperCase() === question.answer.trim().toUpperCase(),
+          question,
+          answer,
+        }],
+      };
+    });
+  }
+
+  render() {
+    const noQuestions = this.state.questionIndex === null;
+    const stats = App.getStats(this.state);
+
+    return (
+      <div>
+        <Progress history={this.state.history} questions={this.state.questions} />
+        <History
+          question={noQuestions ? null : this.state.questions[this.state.questionIndex]}
+          history={this.state.history}
+          processAnswer={this.processAnswer}
+        />
+        {!noQuestions &&
+          <Info stats={stats} words={this.props.words} />
         }
-
-        function createEnglishQuestion(word) {
-            return {
-                text: word.english + (word.englishComment || ""),
-                answer: word.spanish,
-            };
+        {noQuestions &&
+          <FinalInfo stats={stats} words={this.props.words} />
         }
-
-        let newRatio = 1;
-        let oldRatio = 1;
-        switch (mode){
-            case "new":
-                newRatio = 10;
-                oldRatio = 0;
-                break;
-
-            case "mix":
-                newRatio = 5;
-                oldRatio = 1;
-                break;
-
-            case "test":
-                newRatio = 1;
-                oldRatio = 1;
-                break;
-        }
-
-        let questions = [];
-
-        for (let i = 0; i < (words.new || []).length; i++) {
-            for (let c = 0; c < newRatio; c++) {
-                questions.push(createSpanishQuestion(words.new[i]));
-                questions.push(createEnglishQuestion(words.new[i]));
-            }
-        }
-
-        for (let i = 0; i < (words.old || []).length; i++) {
-            for (let c = 0; c < oldRatio; c++) {
-                questions.push(createSpanishQuestion(words.old[i]));
-                questions.push(createEnglishQuestion(words.old[i]));
-            }
-        }
-
-        return questions;
-    }
-
-    static getStats({questions, history}) {
-        const total = history.length;
-        const correct = history.reduce(function(accum, record) { return accum + record.isCorrectAnswer ? 1 : 0; }, 0);
-        const percent = total === 0 ? null : correct / total;
-        const grade = 
-                percent === null ? null :
-                percent >= 0.94 ? "A" :
-                percent >= 0.90 ? "A-" :
-                percent >= 0.87 ? "B+" :
-                percent >= 0.83 ? "B" :
-                percent >= 0.80 ? "B-" :
-                percent >= 0.77 ? "C+" :
-                percent >= 0.73 ? "C" :
-                percent >= 0.70 ? "C-" :
-                percent >= 0.67 ? "D+" :
-                percent >= 0.60 ? "D" :
-                                  "F"
-        ;
-        
-        return {
-            total,
-            correct,
-            percentInfo: percent === null ? "" : (percent * 100).toFixed(0) + "% correct",
-            grade,
-            gradeClass: 
-                percent === null ? "" :
-                grade.startsWith("A") ? "label label-success" :
-                grade.startsWith("B") ? "label label-info" :
-                grade.startsWith("C") ? "label label-warning" :
-                                        "label label-danger"
-        };
-    }
-
-    switchTo(mode){
-        return (prevState, props) => {
-            const questions = App.generateQuestions(props, mode);
-            const questionIndex = App.generateQuestionIndex(questions);
-
-            return {
-                mode,
-                questions,
-                questionIndex,
-                history: []
-            };
-        };
-    }
-
-    processAnswer(answer){
-        this.setState((prevState) => {
-            var question = prevState.questions[prevState.questionIndex];
-
-            var newQuestions = [...prevState.questions];
-            newQuestions.splice(prevState.questionIndex, 1);
-
-            return {
-                questions: newQuestions, 
-                questionIndex: App.generateQuestionIndex(newQuestions),
-                history: [...prevState.history, {
-                    isCorrectAnswer: answer.trim().toUpperCase() === question.answer.trim().toUpperCase(),
-                    question: question,
-                    answer: answer,
-                }]
-            };
-        });
-    }
-
-    render(){
-        const noQuestions = this.state.questionIndex === null;
-
-        const stats = App.getStats(this.state);
-
-        return (
-            <div>
-                <Progress history={this.state.history} questions={this.state.questions} />
-                <History 
-                    question={noQuestions ? null : this.state.questions[this.state.questionIndex] }
-                    history={this.state.history}
-                    processAnswer={this.processAnswer}
-                />
-                {!noQuestions &&
-                    <Info stats={stats} words={this.props.words}/>
-                }
-                {noQuestions &&
-                     <FinalInfo stats={stats} words={this.props.words}/>
-                }
-            </div>
-        );
-    }
+      </div>
+    );
+  }
 }
--- app/words.jsx
+++ app/words.jsx
@@ -1,61 +1,60 @@
-export const words = {
-    new: [
-        { spanish: "gracias", english: "thanks" },
-        { spanish: "bueno", english: "good" },
-        { spanish: "amigo", english: "friend" },
-        { spanish: "por favor", english: "please" },
-        { spanish: "ahora", english: "now" },
-        { spanish: "feliz", english: "happy" },
-        { spanish: "todo", english: "all" },
-        { spanish: "mismo", english: "same" },
-        { spanish: "muy", english: "very" },
-        { spanish: "nunca", english: "never" }
-    ],
+export default {
+  new: [
+    { spanish: 'gracias', english: 'thanks' },
+    { spanish: 'bueno', english: 'good' },
+    { spanish: 'amigo', english: 'friend' },
+    { spanish: 'por favor', english: 'please' },
+    { spanish: 'ahora', english: 'now' },
+    { spanish: 'feliz', english: 'happy' },
+    { spanish: 'todo', english: 'all' },
+    { spanish: 'mismo', english: 'same' },
+    { spanish: 'muy', english: 'very' },
+    { spanish: 'nunca', english: 'never' },
+  ],
+  old: [
+    // -- AR verb ----------------------------
+    { spanish: 'estar', english: 'to be', englishComment: ' (location, feeling)' },
+    { spanish: 'pensar', english: 'to think' },
+    { spanish: 'llevar', english: 'to wear' },
+    { spanish: 'amar', english: 'to love' },
+    { spanish: 'gustar', english: 'to like', englishComment: ' (to be pleasing)' },
 
-    old: [
-        // -- AR verb ----------------------------
-        { spanish: "estar", english: "to be", englishComment: " (location, feeling)" },
-        { spanish: "pensar", english: "to think" },
-        { spanish: "llevar", english: "to wear" },
-        { spanish: "amar", english: "to love" },
-        { spanish: "gustar", english: "to like", englishComment: " (to be pleasing)" },
+    { spanish: 'dar', english: 'to give' },
+    { spanish: 'empezar', english: 'to begin' },
+    { spanish: 'andar', english: 'to walk' },
+    { spanish: 'trabajar', english: 'to work' },
+    { spanish: 'escuchar', english: 'to listen' },
 
-        { spanish: "dar", english: "to give" },
-        { spanish: "empezar", english: "to begin" },
-        { spanish: "andar", english: "to walk" },
-        { spanish: "trabajar", english: "to work" },
-        { spanish: "escuchar", english: "to listen" },
+    { spanish: 'echar', english: 'to throw' },
+    { spanish: 'sacar', english: 'to take out' },
+    { spanish: 'encontrar', english: 'to find' },
 
-        { spanish: "echar", english: "to throw" },
-        { spanish: "sacar", english: "to take out" },
-        { spanish: "encontrar", english: "to find" },
+    // -- ER verb ----------------------------
+    { spanish: 'ser', english: 'to be', englishComment: ' (somebody)' },
+    { spanish: 'hacer', english: 'to do' },
+    { spanish: 'haber', english: 'to have', englishComment: ' (auxiliary, supportive verb)' },
+    { spanish: 'saber', english: 'to know', englishComment: ' (fact or skill)' },
+    { spanish: 'querer', english: 'to want' },
 
-        // -- ER verb ----------------------------
-        { spanish: "ser", english: "to be", englishComment: " (somebody)" },
-        { spanish: "hacer", english: "to do" },
-        { spanish: "haber", english: "to have", englishComment: " (auxiliary, supportive verb)" },
-        { spanish: "saber", english: "to know", englishComment: " (fact or skill)" },
-        { spanish: "querer", english: "to want" },
+    { spanish: 'poder', english: 'to be able' },
+    { spanish: 'poner', english: 'to put' },
+    { spanish: 'deber', english: 'to owe' },
+    { spanish: 'leer', english: 'to read' },
+    { spanish: 'comer', english: 'to eat' },
 
-        { spanish: "poder", english: "to be able" },
-        { spanish: "poner", english: "to put" },
-        { spanish: "deber", english: "to owe" },
-        { spanish: "leer", english: "to read" },
-        { spanish: "comer", english: "to eat" },
+    { spanish: 'beber', english: 'to drink' },
+    { spanish: 'tener', english: 'to have', englishComment: ' (to possess)' },
+    { spanish: 'ver', english: 'to see' },
+    { spanish: 'conocer', english: 'to know', englishComment: ' (person or place)' },
+    { spanish: 'entender', english: 'to understand' },
 
-        { spanish: "beber", english: "to drink" },
-        { spanish: "tener", english: "to have", englishComment: " (to possess)" },
-        { spanish: "ver", english: "to see" },
-        { spanish: "conocer", english:"to know", englishComment: " (person or place)" },
-        { spanish: "entender", english:"to understand" },
+    { spanish: 'ver', english: 'to see' },
+    { spanish: 'creer', english: 'to believe' },
 
-        { spanish: "ver", english: "to see" },
-        { spanish: "creer", english: "to believe" },
-
-        // -- IR verb ----------------------------
-        { spanish: "ir", english: "to go" },
-        { spanish: "salir", english: "to leave" },
-        { spanish: "venir", english: "to come" },
-        { spanish: "decir", english: "to say" },
-    ]
+    // -- IR verb ----------------------------
+    { spanish: 'ir', english: 'to go' },
+    { spanish: 'salir', english: 'to leave' },
+    { spanish: 'venir', english: 'to come' },
+    { spanish: 'decir', english: 'to say' },
+  ],
 };
