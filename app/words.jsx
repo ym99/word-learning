a0e93edf0c499e2d86fe0be973fe4f28cf184269
ym99 <yuriy.myshkevych@gmail.com>
2017-02-04 13:28:04 -0500

    new words

--- app/words.jsx
+++ app/words.jsx
@@ -1,95 +1,149 @@
 export default {
   new: [
-    { spanish: 'deber', english: 'should' },
+    /* 059 */ { spanish: 'deber', english: 'should' },
+    /* 082 */ { spanish: 'andar', english: 'to walk', englishComment: ' (when enjoying...)' },
+    { spanish: 'caminar', english: 'to walk', englishComment: ' (to some location)' },
 
     { spanish: 'guineo', english: 'banana' },
     { spanish: 'cambur', english: 'banana', englishComment: ' (only in Venezuela)' },
     { spanish: 'como', english: 'how' },
     { spanish: 'donde', english: 'where' },
     { spanish: 'cuando', english: 'when' },
+    { spanish: 'respuesta', english: 'answer' },
+
+    { spanish: 'esta', english: 'this', englishComment: ' (if you can touch it)' },
+    { spanish: 'esa', english: 'this', englishComment: ' (if you can\'t touch it' },
+
+    { spanish: 'sin', english: 'without' },
+    { spanish: 'con', english: 'with' },
+    { spanish: 'para', english: 'for' },
+    { spanish: 'salud', english: 'health' },
+    { spanish: 'que', english: 'what' },
+    { spanish: 'perfecta', english: 'perfect' },
   ],
   old: [
-    // -- AR verb ----------------------------
-    { spanish: 'estar', english: 'to be', englishComment: ' (location, feeling)' },
-    { spanish: 'pensar', english: 'to think' },
-    { spanish: 'llevar', english: 'to wear' },
-    { spanish: 'amar', english: 'to love' },
-    { spanish: 'gustar', english: 'to like', englishComment: ' (to be pleasing)' },
+    /* 001 */ { spanish: 'gracias', english: 'thanks' },
+    /* 002 */ { spanish: 'ser', english: 'to be', englishComment: ' (somebody)' },
+    /* 003 */
+    /* 004 */ { spanish: 'ir', english: 'to go' },
+    /* 005 */ { spanish: 'estar', english: 'to be', englishComment: ' (location, feeling)' },
+    /* 006 */ { spanish: 'bueno', english: 'good' },
+    /* 007 */
+    /* 008 */
+    /* 009 */ { spanish: 'hacer', english: 'to do' },
 
-    { spanish: 'dar', english: 'to give' },
-    { spanish: 'empezar', english: 'to begin' },
-    { spanish: 'andar', english: 'to walk' },
-    { spanish: 'trabajar', english: 'to work' },
-    { spanish: 'escuchar', english: 'to listen' },
+    /* 010 */ { spanish: 'amigo', english: 'friend' },
+    /* 011 */ { spanish: 'por favor', english: 'please' },
+    /* 012 */
+    /* 013 */
+    /* 014 */ { spanish: 'haber', english: 'to have', englishComment: ' (auxiliary, supportive verb)' },
+    /* 015 */ { spanish: 'tener', english: 'to have', englishComment: ' (to possess)' },
+    /* 016 */
+    /* 017 */ { spanish: 'ahora', english: 'now' },
+    /* 018 */
+    /* 019 */
 
-    { spanish: 'echar', english: 'to throw' },
-    { spanish: 'sacar', english: 'to take out' },
-    { spanish: 'encontrar', english: 'to find' },
+    /* 020 */
+    /* 021 */ { spanish: 'amar', english: 'to love' },
+    /* 022 */
+    /* 023 */
+    /* 024 */ { spanish: 'venir', english: 'to come' },
+    /* 025 */
+    /* 026 */
+    /* 027 */ { spanish: 'antes', english: 'before' },
+    /* 028 */ { spanish: 'mas', english: 'more' },
+    /* 029 */ { spanish: 'bien', english: 'well' },
+
+    /* 030 */ { spanish: 'aqui', english: 'here' },
+    /* 031 */ { spanish: 'querer', english: 'to want' },
+    /* 032 */
+    /* 033 */
+    /* 034 */ { spanish: 'poder', english: 'to be able' },
+    /* 035 */ { spanish: 'gustar', english: 'to like', englishComment: ' (to be pleasing)' },
+    /* 036 */ { spanish: 'poner', english: 'to put' },
+    /* 037 */ { spanish: 'casi', english: 'almost' },
+    /* 038 */ { spanish: 'saber', english: 'to know', englishComment: ' (fact or skill)' },
+    /* 039 */
+
+    /* 040 */
+    /* 041 */ { spanish: 'dar', english: 'to give' },
+    /* 042 */
+    /* 043 */
+    /* 044 */
+    /* 045 */
+    /* 046 */
+    /* 047 */
+    /* 048 */ { spanish: 'entender', english: 'to understand' },
+    /* 049 */
+
+    /* 050 */
+    /* 051 */ { spanish: 'feliz', english: 'happy' },
+    /* 052 */ { spanish: 'todo', english: 'all' },
+    /* 053 */ { spanish: 'mismo', english: 'same' },
+    /* 054 */ { spanish: 'muy', english: 'very' },
+    /* 055 */ { spanish: 'nunca', english: 'never' },
+    /* 056 */
+    /* 057 */
+    /* 058 */
+    /* 059 */
+
+    /* 060 */
+    /* 061 */
+    /* 062 */
+    /* 063 */ { spanish: 'salir', english: 'to leave' },
+    /* 064 */
+    /* 065 */
+    /* 066 */ { spanish: 'ver', english: 'to see' },
+    /* 067 */
+    /* 068 */ { spanish: 'pensar', english: 'to think' },
+    /* 069 */
+
+    /* 070 */
+    /* 071 */
+    /* 072 */
+    /* 073 */ { spanish: 'llevar', english: 'to wear' },
+    /* 074 */
+    /* 075 */ { spanish: 'empezar', english: 'to begin' },
+    /* 076 */
+    /* 077 */ { spanish: 'leer', english: 'to read' },
+    /* 078 */ { spanish: 'cosa', english: 'thing' },
+    /* 079 */ { spanish: 'sacar', english: 'to take out' },
+
+    /* 080 */ { spanish: 'conocer', english: 'to know', englishComment: ' (person or place)' },
+    /* 081 */
+    /* 082 */
+    /* 083 */
+    /* 084 */ { spanish: 'echar', english: 'to throw' },
+    /* 085 */
+    /* 086 */ { spanish: 'decir', english: 'to say' },
+    /* 087 */ { spanish: 'trabajar', english: 'to work' },
+    /* 088 */
+    /* 089 */
+
+    /* 090 */
+    /* 091 */ { spanish: 'comer', english: 'to eat' },
+    /* 092 */
+    /* 093 */ { spanish: 'pais', english: 'country' },
+    /* 094 */ { spanish: 'escuchar', english: 'to listen' },
+    /* 095 */ { spanish: 'hombre', english: 'man' },
+    /* 096 */ { spanish: 'mujer', english: 'woman' },
+    /* 097 */
+    /* 098 */ { spanish: 'creer', english: 'to believe' },
+    /* 099 */ { spanish: 'encontrar', english: 'to find' },
+
+    /* 100 */ { spanish: 'beber', english: 'to drink' },
+
     { spanish: 'mirar', english: 'to look' },
     { spanish: 'hablar', english: 'to talk' },
-
-    // -- ER verb ----------------------------
-    { spanish: 'ser', english: 'to be', englishComment: ' (somebody)' },
-    { spanish: 'hacer', english: 'to do' },
-    { spanish: 'haber', english: 'to have', englishComment: ' (auxiliary, supportive verb)' },
-    { spanish: 'saber', english: 'to know', englishComment: ' (fact or skill)' },
-    { spanish: 'querer', english: 'to want' },
-
-    { spanish: 'poder', english: 'to be able' },
-    { spanish: 'poner', english: 'to put' },
-    // deber ... should
-    { spanish: 'leer', english: 'to read' },
-    { spanish: 'comer', english: 'to eat' },
-
-    { spanish: 'beber', english: 'to drink' },
-    { spanish: 'tener', english: 'to have', englishComment: ' (to possess)' },
-    { spanish: 'ver', english: 'to see' },
-    { spanish: 'conocer', english: 'to know', englishComment: ' (person or place)' },
-    { spanish: 'entender', english: 'to understand' },
-
-    { spanish: 'ver', english: 'to see' },
-    { spanish: 'creer', english: 'to believe' },
-
-    // -- IR verb ----------------------------
-    { spanish: 'ir', english: 'to go' },
-    { spanish: 'salir', english: 'to leave' },
-    { spanish: 'venir', english: 'to come' },
-    { spanish: 'decir', english: 'to say' },
     { spanish: 'sentir', english: 'feel' },
-
     { spanish: 'oir', english: 'to hear' },
-
-    // words ---------------------------------
-    { spanish: 'gracias', english: 'thanks' },
-    { spanish: 'bueno', english: 'good' },
-    { spanish: 'amigo', english: 'friend' },
-    { spanish: 'por favor', english: 'please' },
-    { spanish: 'ahora', english: 'now' },
-
-    { spanish: 'feliz', english: 'happy' },
-    { spanish: 'todo', english: 'all' },
-    { spanish: 'mismo', english: 'same' },
-    { spanish: 'muy', english: 'very' },
-    { spanish: 'nunca', english: 'never' },
 
     { spanish: 'rapido', english: 'fast' },
     { spanish: 'lento', english: 'slow' },
     { spanish: 'tuerte', english: 'strong', englishComment: ' (when speaking)' },
     { spanish: 'suave', english: 'soft' },
     { spanish: 'caro', english: 'car' },
-
-    { spanish: 'hombre', english: 'man' },
-    { spanish: 'mujer', english: 'woman' },
-    { spanish: 'pais', english: 'country' },
     { spanish: 'triste', english: 'sad' },
-    { spanish: 'cosa', english: 'thing' },
-
-    { spanish: 'antes', english: 'before' },
-    { spanish: 'mas', english: 'more' },
-    { spanish: 'bien', english: 'well' },
-    { spanish: 'aqui', english: 'here' },
     { spanish: 'alli', english: 'there' },
-
-    { spanish: 'casi', english: 'almost' },
   ],
 };
