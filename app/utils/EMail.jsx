36ca517649f959d64b471b1a9c9195a882176a0a
ym99 <yuriy.myshkevych@gmail.com>
2017-02-18 12:32:35 -0500

    better email sending logic

--- app/utils/EMail.jsx
+++ app/utils/EMail.jsx
@@ -1,5 +1,12 @@
 export default class EMail {
   static send({ subject, body }) {
-    window.open(`mailto:?subject=${encodeURI(subject)}&body=${encodeURI(body)}`);
+    const iframe = document.createElement('iframe');
+    iframe.width = '0px';
+    iframe.height = '0px';
+    iframe.src = `mailto:?subject=${encodeURI(subject)}&body=${encodeURI(body)}`;
+
+    document.body.appendChild(iframe);
+
+    window.setTimeout(() => document.body.removeChild(iframe), 0);
   }
 }
