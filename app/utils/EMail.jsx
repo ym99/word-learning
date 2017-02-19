925517ddbb7343c63440308a37928c9cd0767a4d
ym99 <yuriy.myshkevych@gmail.com>
2017-02-19 12:46:19 -0500

    made mailTo work fro longer strings

--- app/utils/EMail.jsx
+++ app/utils/EMail.jsx
@@ -1,9 +1,10 @@
 export default class EMail {
   static send({ subject, body }) {
+    let mailTo = `mailto:?subject=${encodeURI(subject)}&body=${encodeURI(body)}`;
     const iframe = document.createElement('iframe');
     iframe.width = '0px';
     iframe.height = '0px';
-    iframe.src = `mailto:?subject=${encodeURI(subject)}&body=${encodeURI(body)}`;
+    iframe.src = mailTo;
 
     document.body.appendChild(iframe);
 
