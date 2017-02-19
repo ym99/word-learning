ed5c5248d183268e842fba8414141aabc33c5365
ym99 <yuriy.myshkevych@gmail.com>
2017-02-19 12:47:27 -0500

    made mailTo work for longer strings

--- app/utils/EMail.jsx
+++ app/utils/EMail.jsx
@@ -1,6 +1,10 @@
 export default class EMail {
   static send({ subject, body }) {
     let mailTo = `mailto:?subject=${encodeURI(subject)}&body=${encodeURI(body)}`;
+    if (mailTo.length > 2048) {
+      mailTo = `${mailTo.substr(0, 2040)}${encodeURI('\n...')}`;
+    }
+
     const iframe = document.createElement('iframe');
     iframe.width = '0px';
     iframe.height = '0px';
