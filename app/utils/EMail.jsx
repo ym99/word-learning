ffd45ab44a4366650a41c66ebe389bcadcd652dd
ym99 <yuriy.myshkevych@gmail.com>
2017-02-24 22:05:20 -0500

    better email

--- app/utils/EMail.jsx
+++ app/utils/EMail.jsx
@@ -2,7 +2,7 @@ export default class EMail {
   static send({ subject, body }) {
     let mailTo = `mailto:?subject=${encodeURI(subject)}&body=${encodeURI(body)}`;
     if (mailTo.length > 2048) {
-      mailTo = `${mailTo.substr(0, 2040)}${encodeURI('\n...')}`;
+      mailTo = `${mailTo.substr(0, 2040)}${encodeURI('\r\n...')}`;
     }
 
     const iframe = document.createElement('iframe');
